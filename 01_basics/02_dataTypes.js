9ed51de719decd2afcf890304460c29d66ace466
k-sontakke <ksontakke@digitalzonein.com>
2025-01-10 18:22:19 +0530

    Update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,10 +1,10 @@
-"use strict"; // treat all JS code as newer version
+"u se strict"; // treat all JS code as newer version
 
 // alert( 3 + 3) // we are using nodejs, not browser
 
-console.log(3 
+console.log(3
     +
-     3) // code readability should be high
+    3) // code readability should be high
 
 console.log("Hitesh")
 
